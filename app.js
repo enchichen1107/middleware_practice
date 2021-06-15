1fe538ac2ee89527a11a4f179ac8c5af587f90d5
enchichen1107 <b03409032@gmail.com>
2021-06-16 00:50:45 +0800

    feat: demo middleware on frontpage

--- app.js
+++ app.js
@@ -1,23 +1,39 @@
+// init packages
 const express = require('express')
+const exphbs = require('express-handlebars')
 const app = express()
 const port = 3000
+let middleware = ''
+
+// set view engine
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs')
+
+// testing routers
+// middleware
+app.use(function (req, res, next) {
+  middleware = `${Date.now()} | ${req.method} from ${req.originalUrl}`
+  console.log(middleware)
+  next()
+})
 
 app.get('/', (req, res) => {
-  res.send('列出全部 Todo')
+  res.render('index', { middleware })
 })
 
 app.get('/new', (req, res) => {
-  res.send('新增 Todo 頁面')
+  res.render('index', { middleware })
 })
 
 app.get('/:id', (req, res) => {
-  res.send('顯示一筆 Todo')
+  res.render('index', { middleware })
 })
 
 app.post('/', (req, res) => {
-  res.send('新增一筆  Todo')
+  res.render('index', { middleware })
 })
 
+// listen on port
 app.listen(port, () => {
   console.log(`App running on http://localhost:${port}`)
 })
